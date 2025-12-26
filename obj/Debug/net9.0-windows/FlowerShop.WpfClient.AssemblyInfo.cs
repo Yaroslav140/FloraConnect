85634666b494ccd5556c609128b0ccde6e22eaa2
yarik <paramonovjaroslav1245@gmail.com>
2025-12-26 12:41:41 +0300

    none update

--- obj/Debug/net9.0-windows/FlowerShop.WpfClient.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/FlowerShop.WpfClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlowerShop.WpfClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc25f3440ae35d9472d51e7a4e39c9665063d2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96d3d4118171b9affe2c7dda6acadd2dbafe9f6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlowerShop.WpfClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlowerShop.WpfClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
